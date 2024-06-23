ab526759e842736237b7b4aa214b59c7dd8b3658
Artem Kovalevskyi <artem4852@MacBook-Pro--Artem.local>
2024-06-23 11:27:02 +0200

    Stop tracking ignored files

--- extract_products.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open("products.json", "r") as f: products = json.load(f)["pageProps"]["availableItems"]
-
-for product in products:
-    print(product["Name"] + ((" " + product["Small Name"]) if product["Small Name"] else ""), product["Max Order Quantity"])
